6707f3e11c80202e4689c5fd12d9d64d628fa06e
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2024-03-20 18:43:01 +0200

    Create chunks for build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	base: "/",
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+						return id.toString().split("node_modules/")[1].split("/")[0].toString();
+					}
+				},
+			},
+		},
+	},
 });
